e8ebaeb124417b248f88cf84c8299e2ea1f91336
Erick Crowne <erickcrowne85@gmail.com>
2026-03-03 06:41:09 -0600

    msg

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,6 +3,11 @@ import { useNavigate } from 'react-router-dom'
 export default function MovieCard({movie}) {
     const navigate = useNavigate()
     const TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500'
+    const PLACEHOLDER_IMAGE = '/placeholder.jpg'
+    
+    const imageUrl = movie.poster_path 
+      ? `${TMDB_IMAGE_BASE_URL}${movie.poster_path}` 
+      : PLACEHOLDER_IMAGE
     
     return (
           <div 
@@ -11,8 +16,9 @@ export default function MovieCard({movie}) {
             onClick={() => navigate(`/movie/${movie.id}`)}
           >
             <img 
-              src={`${TMDB_IMAGE_BASE_URL}${movie.poster_path}`} 
+              src={imageUrl} 
               alt={movie.title}
+              onError={(e) => { e.target.src = PLACEHOLDER_IMAGE }}
             />
             {/* <div className="movie-card-title">
               <h3>Rating: <span>{movie.vote_average.toFixed(1)}</span></h3>
